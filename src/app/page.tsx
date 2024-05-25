267116e9efd424d2878a1427e7d80b0759c66f50
Lukas Stauersboel <lukas.stauersbol@gmail.com>
2024-05-25 20:18:14 +0200

    feat(app): add Kanban component to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 
+import { Kanban } from "./_components/kanban";
 import { TopBar } from "./_components/top-bar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-gray-800">
+    <main className="flex min-h-screen flex-col overflow-y-hidden bg-gray-800">
       <TopBar />
+      <Kanban />
     </main>
   );
 }
