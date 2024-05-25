6eb4728dec5b8c72a8c5628dc6e66610da102799
Lukas Stauersboel <lukas.stauersbol@gmail.com>
2024-05-25 17:20:46 +0200

    feat(app): add top-bar component

---
+++ src/app/_components/top-bar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { CreateLaws } from './create-laws'
+
+export const TopBar = () => {
+    return (
+        <nav className="flex justify-end p-3 items-center bg-gray-700 border-b-gray-600 h-16">
+            <CreateLaws/>
+        </nav>
+    )
+}
+
