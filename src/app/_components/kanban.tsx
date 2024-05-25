f81f2d741b8cab5919b97989e47ce1cd18642c98
Lukas Stauersboel <lukas.stauersbol@gmail.com>
2024-05-25 20:41:04 +0200

    refactor(components): refactor kanban component

--- src/app/_components/kanban.tsx
+++ src/app/_components/kanban.tsx
@@ -1,4 +1,6 @@
 import { api } from "~/trpc/server"
+import { KanbanItem } from "./kanban-item";
+import { KanbanCategory } from "./kanban-category";
 
 export const Kanban = async () => {
 
@@ -10,17 +12,11 @@ export const Kanban = async () => {
     return (
         <div className="flex h-screen mt-5 space-x-5 mb-5 justify-center items-start">
             {statuses.map(status => (
-                <div className="flex w-72 h-full flex-col rounded-xl" key={status}>
-                    <h2 className="font-bold text-white text-xl">{status}</h2>
-                    <div className="overflow-y-auto space-y-5 overflow-x-hidden" style={{ scrollbarWidth: 'thin' }}>
-                        {laws.filter(law => law.status === status).map(law => (
-                            <div className="bg-slate-600 rounded-xl hyphens-auto p-5" key={law.id}>
-                                <p className="bg-red-800 inline-block px-3 mb-3 rounded-xl text-white">{law.number}</p>
-                                <p className="text-white" lang="da" key={law.id}>{law.title}</p>
-                            </div>
-                        ))}
-                    </div>
-                </div>
+                <KanbanCategory key={status} status={status}>
+                    {laws.filter(law => law.status === status).map(law => (
+                        <KanbanItem key={law.id} lawNumber={law.number} lawTitle={law.title} />
+                    ))}
+                </KanbanCategory>
             ))}
         </div>
     )
